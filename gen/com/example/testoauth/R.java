b9fcdceb008420e9a4f171254b2bf43a3d5a9e59
gaozhuqing <95233021@qq.com>
2016-05-06 16:44:30 +0800

    “修改了很多"

--- gen/com/example/testoauth/R.java
+++ gen/com/example/testoauth/R.java
@@ -11,74 +11,148 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int black=0x7f040005;
-        public static final int blue=0x7f040003;
-        public static final int gray=0x7f040006;
-        public static final int gray_dark=0x7f040009;
-        public static final int gray_light=0x7f040008;
-        public static final int gray_white=0x7f040007;
-        public static final int green=0x7f040001;
-        public static final int green_dark=0x7f040002;
-        public static final int list_divider=0x7f04000d;
-        public static final int orange=0x7f04000c;
-        public static final int pink=0x7f04000b;
-        public static final int red=0x7f040000;
-        public static final int transparent=0x7f04000e;
-        public static final int transparent_bg=0x7f04000f;
-        public static final int white=0x7f040004;
-        public static final int yellow=0x7f04000a;
+        public static int black=0x7f040005;
+        public static int blue=0x7f040003;
+        public static int gray=0x7f040006;
+        public static int gray_dark=0x7f040009;
+        public static int gray_light=0x7f040008;
+        public static int gray_white=0x7f040007;
+        public static int green=0x7f040001;
+        public static int green_dark=0x7f040002;
+        public static int list_divider=0x7f04000d;
+        /**  桔黄色 
+         */
+        public static int moccasin=0x7f040010;
+        public static int orange=0x7f04000c;
+        public static int pink=0x7f04000b;
+        public static int red=0x7f040000;
+        public static int transparent=0x7f04000e;
+        public static int transparent_bg=0x7f04000f;
+        public static int white=0x7f040004;
+        public static int yellow=0x7f04000a;
     }
     public static final class dimen {
-        /**  Default screen margins, per the Android Design guidelines. 
-
+        /** 
          Example customization of dimensions originally defined in res/values/dimens.xml
          (such as screen margins) for screens with more than 820dp of available width. This
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static int activity_horizontal_margin=0x7f050002;
+        public static int shadow_width=0x7f050001;
+        /**  slidingmenu配置 
+         */
+        public static int slidingmenu_offset=0x7f050000;
     }
     public static final class drawable {
-        public static final int btn_blue_light_f=0x7f020000;
-        public static final int btn_blue_light_n=0x7f020001;
-        public static final int btn_white_f=0x7f020002;
-        public static final int btn_white_n=0x7f020003;
-        public static final int btn_yellow_f=0x7f020004;
-        public static final int btn_yellow_n=0x7f020005;
-        public static final int button_selector_blue_light=0x7f020006;
-        public static final int button_selector_white=0x7f020007;
-        public static final int button_selector_yellow=0x7f020008;
-        public static final int clear_icon=0x7f020009;
-        public static final int clear_icon_f=0x7f02000a;
-        public static final int clear_icon_n=0x7f02000b;
-        public static final int ic_launcher=0x7f02000c;
+        public static int app_bg=0x7f020000;
+        public static int back_f=0x7f020001;
+        public static int back_n=0x7f020002;
+        public static int btn_blue_light_f=0x7f020003;
+        public static int btn_blue_light_n=0x7f020004;
+        public static int btn_green_f=0x7f020005;
+        public static int btn_green_n=0x7f020006;
+        public static int btn_white_f=0x7f020007;
+        public static int btn_white_n=0x7f020008;
+        public static int btn_yellow_f=0x7f020009;
+        public static int btn_yellow_n=0x7f02000a;
+        public static int button_selector_back=0x7f02000b;
+        public static int button_selector_blue_light=0x7f02000c;
+        public static int button_selector_green=0x7f02000d;
+        public static int button_selector_menu=0x7f02000e;
+        public static int button_selector_white=0x7f02000f;
+        public static int button_selector_yellow=0x7f020010;
+        public static int clear_icon=0x7f020011;
+        public static int clear_icon_f=0x7f020012;
+        public static int clear_icon_n=0x7f020013;
+        public static int ic_launcher=0x7f020014;
+        public static int ic_sunshine=0x7f020015;
+        public static int left_bg=0x7f020016;
+        public static int left_list_group_bg=0x7f020017;
+        public static int left_list_item_bg=0x7f020018;
+        public static int left_list_item_f=0x7f020019;
+        public static int line=0x7f02001a;
+        public static int list_divider=0x7f02001b;
+        public static int menu_f=0x7f02001c;
+        public static int menu_n=0x7f02001d;
+        public static int photo01=0x7f02001e;
+        public static int recommend=0x7f02001f;
+        public static int roller=0x7f020020;
+        public static int shadow=0x7f020021;
+        public static int share=0x7f020022;
+        public static int top_bg=0x7f020023;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000c;
-        public static final int btnLayout=0x7f090009;
-        public static final int clearName=0x7f090002;
-        public static final int clearPwd=0x7f090005;
-        public static final int layout01=0x7f090000;
-        public static final int layout02=0x7f090003;
-        public static final int layout03=0x7f090006;
-        public static final int loginBtn=0x7f09000a;
-        public static final int login_check=0x7f090007;
-        public static final int pwdBtn=0x7f090008;
-        public static final int registerBtn=0x7f09000b;
-        public static final int userName=0x7f090001;
-        public static final int userPwd=0x7f090004;
+        public static int actionBarTitle=0x7f090001;
+        public static int actionBarUp=0x7f090000;
+        public static int action_settings=0x7f090029;
+        public static int btnLayout=0x7f09000f;
+        public static int cacheClearBtn=0x7f090021;
+        public static int clearName=0x7f090008;
+        public static int clearPwd=0x7f09000b;
+        public static int content_frame=0x7f090027;
+        public static int cursor=0x7f090004;
+        public static int desktop_list_child_icon=0x7f090022;
+        public static int desktop_list_child_name=0x7f090023;
+        public static int desktop_list_group_name=0x7f090024;
+        public static int imageView1=0x7f090016;
+        public static int launcher=0x7f090012;
+        public static int launcherView=0x7f090013;
+        public static int layout01=0x7f090006;
+        public static int layout02=0x7f090009;
+        public static int layout03=0x7f09000c;
+        public static int localimage=0x7f090019;
+        public static int locallist=0x7f090018;
+        public static int locallistitem=0x7f09001a;
+        public static int loginBtn=0x7f090010;
+        public static int login_check=0x7f09000d;
+        public static int login_layout=0x7f09001b;
+        public static int menu_frame=0x7f090028;
+        public static int menu_list=0x7f090020;
+        public static int netlist=0x7f090025;
+        public static int netlistitem=0x7f090026;
+        public static int pwdBtn=0x7f09000e;
+        public static int registerBtn=0x7f090011;
+        public static int sunshineView=0x7f09001e;
+        public static int textView1=0x7f090014;
+        public static int textView2=0x7f090015;
+        public static int tv_guid1=0x7f090002;
+        public static int tv_guid2=0x7f090003;
+        public static int userName=0x7f090007;
+        public static int userPwd=0x7f09000a;
+        public static int user_name=0x7f09001d;
+        public static int user_photo=0x7f09001c;
+        public static int user_point=0x7f09001f;
+        public static int version=0x7f090017;
+        public static int viewpager=0x7f090005;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static int actionbar_crime_fragment=0x7f030000;
+        public static int actionbar_main=0x7f030001;
+        public static int activity_main=0x7f030002;
+        public static int launcher=0x7f030003;
+        public static int local_list=0x7f030004;
+        public static int local_list_item=0x7f030005;
+        public static int main_menu=0x7f030006;
+        public static int main_menu_list_child=0x7f030007;
+        public static int main_menu_list_group=0x7f030008;
+        public static int network_list=0x7f030009;
+        public static int network_list_item=0x7f03000a;
+        public static int sliding_menu_content=0x7f03000b;
+        public static int sliding_menu_menu=0x7f03000c;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060002;
-        public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060001;
+        public static int action_settings=0x7f060003;
+        public static int app_name=0x7f060000;
+        public static int back=0x7f060007;
+        public static int clear_cache=0x7f060005;
+        public static int copyright=0x7f060004;
+        public static int demo_name=0x7f060006;
+        public static int hello_world=0x7f060002;
+        public static int logo_name=0x7f060001;
     }
     public static final class style {
         /** 
@@ -102,10 +176,12 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static int AppTheme=0x7f070001;
+        public static int CustomWindowTitleBackground=0x7f070002;
+        public static int test=0x7f070003;
     }
 }
